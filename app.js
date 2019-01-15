22f023cf90e837136c047a02acd442f17bfc2bda
Brandon Wirz <bwirz75@gmail.com>
2019-01-14 21:42:06 -0700

    update code in progress second commit

--- app.js
+++ app.js
@@ -10,5 +10,28 @@ function initMap()  {
 }
 
 function apiData() {
-  
+  $('.search-results').html(''); // Clear Search Results
+  $('#map').html('');
+
+  // Get Input from the form
+    let l = $('#city').val();
+
+    $.ajax({
+        url: "https://api.eventful.com/json/events/search",
+        type: "GET",
+        data: {
+            app_key: "",
+            q: "music",
+        },
+        crossDomain: true,
+        dataType: 'jsonp'
+    }).then(function(data) {
+
+        initialize(data);
+
+        for (let i = 0; i < data.events.event.length; i++) {
+            let output = getOutput(data.events.event[i]);// Get Output
+            $('.search-results').append(output);// Display Results
+        }
+    });
 }
